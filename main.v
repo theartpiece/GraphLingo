d7834d246516e9527f72af3370363c4ccfcfacc4
Chitrank Gupta <chitrankgupta@Chitranks-Air.lan>
2024-03-16 23:03:16 -0500

    stuck in 'get_all_reachable_nodes function

--- main.v
+++ main.v
@@ -35,14 +35,20 @@ Qed.
 
 
 (* TODO --> check that we are not adding a node that's larger than n *)
-Fixpoint construct_graph (n: nat) (edge_list : list Edge): Graph := 
+Fixpoint construct_undir_graph (n: nat) (edge_list : list Edge): Graph := 
     match edge_list with 
     | nil => (construct_empty_graph n)
-    | (cons (u,v) edge_list1) => add_edge (construct_graph n edge_list1) u v
+    | (cons (u,v) edge_list1) => add_edge (add_edge (construct_undir_graph n edge_list1) u v) v u
+    end.
+
+Fixpoint construct_bidir_graph (n: nat) (edge_list : list Edge): Graph := 
+    match edge_list with 
+    | nil => (construct_empty_graph n)
+    | (cons (u,v) edge_list1) => add_edge (construct_bidir_graph n edge_list1) u v
     end.
 
 Example null_graph:=
-    construct_graph 0 nil.
+    construct_undir_graph 0 nil.
 Example null_graph_is_null:
     null_graph = nil.
 Proof.
@@ -50,23 +56,84 @@ Proof.
 Qed.
 
 
-Example two_cycle:
-    construct_graph 2 [(1,2);(2,1)] = [(2,[1]);(1,[2])].
+Example two_cycle:=
+    construct_undir_graph 2 [(1,2)].
+Example two_cycle_is_correct:
+    two_cycle = [(2,[1]);(1,[2])].
 Proof.
     simpl. auto.
 Qed.
 
+Example one_line:=
+    construct_bidir_graph 2 [(1,2)].
+Example one_line_is_correct:
+    one_line = [(2,[]);(1,[2])].
+Proof.
+    simpl. auto.
+Qed.
+
+
+
+
 (* Todo  ---> Prove that construct_graph function is correct. *)
+
+
+    
+
+Fixpoint get_all_neighbors (g: Graph) (u : Node): (list Node):=
+    match g with
+    | nil => nil
+    | (v,e)::g1 => 
+                if (Nat.eqb u v) 
+                    then e ++ (get_all_neighbors g1 u)
+                else
+                    let n1 := (get_all_neighbors g1 u) in 
+                    if (andb (existsb (Nat.eqb u) e)  (negb (existsb (Nat.eqb v) n1)))
+                        then v::n1
+                    else n1
+    end.
+
+Fixpoint get_actual_neighbors (g: Graph) (u : Node): (list Node):=
+    match g with
+    | nil => nil
+    | (v,e)::g1 => 
+                if (Nat.eqb u v) 
+                    then e 
+                else
+                    get_actual_neighbors g1 u
+    end.
+
+
+Example all_nghbrs_of_1_in_two_cycle:
+    get_all_neighbors two_cycle 1 = [2].
+Proof.
+    simpl. auto.
+Qed.
+
+Example actual_nghbrs_of_1_in_one_line:
+    get_actual_neighbors one_line 1 = [2].
+Proof.
+    simpl. auto.
+Qed.
+
+Example actual_nghbrs_of_2_in_one_line:
+    get_actual_neighbors one_line 2 = nil.
+Proof.
+    simpl. auto.
+Qed.
 
 
 Fixpoint is_connected (g : Graph) : Prop:=
     match g with 
     | nil => True
-    | (v,e)::g1 => match e with
+    | (v,e)::g1 => let n := get_all_neighbors g v in 
+                    match n with
                     | nil => False
                     | _ => is_connected g1
                     end
     end.
+    
+    
 
 
 Example null_graph_is_connected:
@@ -75,11 +142,34 @@ Proof.
     simpl. auto.
 Qed.
 
-Example null_graph_is_not_connected:
-    is_connected null_graph.
+Example one_line_is_connected:
+    is_connected one_line.
 Proof.
     simpl. auto.
 Qed.
+
+Example one_line_one_node:=
+    construct_bidir_graph 3 [(1,2)].
+
+Example one_line_one_node_is_not_connected:
+    not (is_connected one_line_one_node).
+Proof.
+    simpl. auto.
+Qed.
+
+Fixpoint get_reachable_nodes_helper (g:Graph) (list_u: list Node): (list Node) :=
+    match list_u with 
+    | nil => nil
+    | v::l => let rest := (get_reachable_nodes_helper g l ) in 
+                match get_actual_neighbors g v with
+                    | nil => v::rest
+                    | nl => (v::(get_reachable_nodes_helper g nl))++rest
+                end
+    end.
+
+Fixpoint is_cyclic_bidirec (g: Graph) : Prop :=
+
+
 
 
 
