96348cc4f2cdbdf878b600d91092d78e84d97caf
Kavya Varma <kavya.varma98@gmail.com>
2024-04-11 15:39:32 -0500

    Initial attempt at DFS

--- main.v
+++ main.v
@@ -1,9 +1,9 @@
 Require Import String Arith List Lia.
+Require Import Coq.FSets.FMapList.
 Import ListNotations.
-Open Scope string_scope.
 Module Part1.
 (* ToDo: Should Node be [Definition Node] or [Notation Node]? Check HW1 to see what I am talking about *)
-Notation Node := nat.
+Definition Node := nat.
 Definition Edge := (prod Node Node).
 Definition EdgeList := (list Edge).
 (* Type edge. *)
@@ -13,21 +13,20 @@ Definition Graph := list (prod Node (list Node)).
 (* TODO := add the check to avoid duplicated edge addition *)
 Fixpoint add_edge (adj : Graph) (v1 v2: Node): Graph :=
     match adj with
-    | nil => (cons (v1, (cons v2 nil)) nil) 
-    | (cons (x,nghbrs) adj1) => 
-            if (Nat.eqb x v1) 
-            then  (cons (x, (cons v2 nghbrs)) adj1) 
-            else (cons (x, nghbrs) (add_edge adj1 v1 v2)) 
+    | [] => [(v1, [v2])]
+    | (v, adj) :: rest => 
+            if (Nat.eqb v v1) then  (v1, v2 :: adj) :: rest
+            else (v, adj) :: add_edge rest v1 v2  
     end.
 
 Fixpoint construct_empty_graph (n: nat) : Graph :=
     match n with
-    | 0 => nil
-    | (S n1) => (n,nil)::(construct_empty_graph n1)
+    | 0 => []
+    | (S n1) => (n,[])::(construct_empty_graph n1)
     end.
     
 Example two_nodes_empty_graph:
-    construct_empty_graph 2 = [(2,nil);(1,nil)].
+    construct_empty_graph 2 = [(2,[]);(1,[])].
 Proof.
     simpl. auto.
 Qed.
@@ -37,20 +36,20 @@ Qed.
 (* TODO --> check that we are not adding a node that's larger than n *)
 Fixpoint construct_undir_graph (n: nat) (edge_list : list Edge): Graph := 
     match edge_list with 
-    | nil => (construct_empty_graph n)
-    | (cons (u,v) edge_list1) => add_edge (add_edge (construct_undir_graph n edge_list1) u v) v u
+    | [] => (construct_empty_graph n)
+    | (u,v) :: edge_list1 => add_edge (add_edge (construct_undir_graph n edge_list1) u v) v u
     end.
 
 Fixpoint construct_dir_graph (n: nat) (edge_list : list Edge): Graph := 
     match edge_list with 
-    | nil => (construct_empty_graph n)
-    | (cons (u,v) edge_list1) => add_edge (construct_dir_graph n edge_list1) u v
+    | [] => (construct_empty_graph n)
+    | (u,v) :: edge_list1 => add_edge (construct_dir_graph n edge_list1) u v
     end.
 
 Example null_graph:=
-    construct_undir_graph 0 nil.
+    construct_undir_graph 0 [].
 Example null_graph_is_null:
-    null_graph = nil.
+    null_graph = [].
 Proof.
     auto.
 Qed.
@@ -80,44 +79,44 @@ Qed.
 
     
 
-Fixpoint get_all_neighbors (g: Graph) (u : Node): (list Node):=
+Fixpoint get_reachable_nodes (g: Graph) (u : Node): (list Node):=
     match g with
-    | nil => nil
+    | [] => []
     | (v,e)::g1 => 
                 if (Nat.eqb u v) 
-                    then e ++ (get_all_neighbors g1 u)
+                    then e ++ (get_reachable_nodes g1 u)
                 else
-                    let n1 := (get_all_neighbors g1 u) in 
+                    let n1 := (get_reachable_nodes g1 u) in 
                     if (andb (existsb (Nat.eqb u) e)  (negb (existsb (Nat.eqb v) n1)))
                         then v::n1
                     else n1
     end.
 
-Fixpoint get_actual_neighbors (g: Graph) (u : Node): (list Node):=
+Fixpoint get_neighbors (g: Graph) (u : Node): (list Node):=
     match g with
-    | nil => nil
+    | [] => []
     | (v,e)::g1 => 
                 if (Nat.eqb u v) 
                     then e 
                 else
-                    get_actual_neighbors g1 u
+                    get_neighbors g1 u
     end.
 
 
 Example all_nghbrs_of_1_in_two_cycle:
-    get_all_neighbors two_cycle 1 = [2].
+    get_reachable_nodes two_cycle 1 = [2].
 Proof.
     simpl. auto.
 Qed.
 
 Example actual_nghbrs_of_1_in_one_line:
-    get_actual_neighbors one_line 1 = [2].
+    get_neighbors one_line 1 = [2].
 Proof.
     simpl. auto.
 Qed.
 
 Example actual_nghbrs_of_2_in_one_line:
-    get_actual_neighbors one_line 2 = nil.
+    get_neighbors one_line 2 = [].
 Proof.
     simpl. auto.
 Qed.
@@ -126,7 +125,7 @@ Qed.
 Fixpoint is_connected (g : Graph) : Prop:=
     match g with 
     | nil => True
-    | (v,e)::g1 => let n := get_all_neighbors g v in 
+    | (v,e)::g1 => let n := get_reachable_nodes g v in 
                     match n with
                     | nil => False
                     | _ => is_connected g1
@@ -157,19 +156,108 @@ Proof.
     simpl. auto.
 Qed.
 
-(* Fixpoint is_reachable_in_k_steps (u v: Node) (k: nat): Prop :=
-    match k with
-    | 0 => if (Nat.eqb u v) then True else False
-    | S => 
-Fixpoint get_reachable_nodes_helper (g:Graph) (list_u: list Node) (list_seen : list Node): (list Node) :=
-    match list_u with 
-    | nil => nil
-    | v::l => if (existsb (Nat.eqb v) list_seen)
-                    then (get_reachable_nodes_helper g l list_seen)
-                else v::(get_reachable_nodes_helper g ((get_actual_neighbors g v) ++ l ) (v::list_seen))
-    end.
+Inductive Step : Graph * list Node * list Node -> Graph * list Node * list Node -> Prop :=
+| StepVisited : forall g vis st v,
+    ~ In v vis -> Step (g, vis, v :: st) (g, v :: vis, (get_neighbors g v) ++ st)
+| StepVisit : forall g vis st v,
+    In v vis -> Step (g, vis, v :: st) (g, vis, st)
+| StepStart : forall g v,
+    Step (g, [], []) (g, [], [v]).
 
-Fixpoint is_cyclic_direc (g: Graph) : Prop := *)
+Inductive StepStar : Graph * list Node * list Node -> Graph * list Node * list Node -> Prop :=
+| StepRefl : forall g vis st,
+    StepStar (g, vis, st) (g, vis, st)
+| StepStarOnce : forall g vis1 vis2 vis3 st1 st2 st3,
+    Step (g, vis1, st1) (g, vis2, st2) -> StepStar (g, vis2, st2) (g, vis3, st3) -> StepStar (g, vis1, st1) (g, vis3, st3).
+
+(* Lemma item_in_list: forall a l node,
+ In node (a :: l) -> hd node (a :: l) /\ In node l.
+Proof.
+    intros. simpl in H. 
+    
+Qed. *)
+
+Theorem get_reachable_nodes_implies_get_neighbours: forall graph start node,
+    In node (get_neighbors graph start) -> In node (get_reachable_nodes graph start).
+Proof.
+    intros. induction get_neighbors. inversion H. simpl in H. destruct H.
+    - .
+    - auto.   
+    apply in_app_iff in H.
+    
+Qed.
+
+
+Theorem dfs_correctness : forall graph start visited' stack',
+    StepStar (graph, [], [start]) (graph, visited', stack') ->
+    (forall node, In node visited' -> In node (get_reachable_nodes graph node) ).
+Proof.
+    intros. inversion H;subst. 
+    - inversion H0.
+    - inversion H3;subst. simpl In in H0. unfold get_reachable_nodes. 
+    unfold get_neighbors in H3.
+    
+    inversion H2.  unfold get_reachable_nodes. simpl.
+    (* Property: All visited nodes are reachable *)
+    (forall node, In node visited' -> exists visited'' path, StepStar (graph, [], [start]) (graph, visited'', node :: path)).
+  Proof.
+    intros graph start visited stack H.
+    induction H.
+    - (* Base case: Term *)
+      intros. econstructor. admit.
+    - intros. econstructor.  eauto. eapply StepStarOnce. inversion H.
+    - (* Inductive case: Step *)
+      intros node H_in.
+      destruct H0 as [graph' visited' stack' H_step].
+      inversion H_step; subst.
+      + (* Step_Pop *)
+        apply in_app_iff in H_in.
+        destruct H_in as [H_in_stack | H_in_adj].
+        * (* Node is in stack *)
+          apply IHclos_refl_trans. assumption.
+        * (* Node is in adjacency list *)
+          admit. (* You need to prove the existence of a path to this node *)
+      + (* Step_Visit *)
+        apply IHclos_refl_trans. assumption.
+  Admitted.
+
+Theorem dfs_correctness : forall graph start visited' stack',
+    StepStar (graph, [], [start]) (graph, visited', stack') ->
+    (* Property: All visited nodes are reachable *)
+    (forall node, In node visited' -> exists visited'' path, StepStar (graph, [], [start]) (graph, visited'', node :: path)).
+  Proof.
+    intros graph start visited stack H.
+    induction H.
+    - (* Base case: Term *)
+      intros. econstructor. admit.
+    - intros. econstructor.  eauto. eapply StepStarOnce. inversion H.
+    - (* Inductive case: Step *)
+      intros node H_in.
+      destruct H0 as [graph' visited' stack' H_step].
+      inversion H_step; subst.
+      + (* Step_Pop *)
+        apply in_app_iff in H_in.
+        destruct H_in as [H_in_stack | H_in_adj].
+        * (* Node is in stack *)
+          apply IHclos_refl_trans. assumption.
+        * (* Node is in adjacency list *)
+          admit. (* You need to prove the existence of a path to this node *)
+      + (* Step_Visit *)
+        apply IHclos_refl_trans. assumption.
+  Admitted.
+
+Inductive Term : Graph * list Node * list Node -> Prop :=
+| Term_Empty : forall g, Term (g, [], []).
+
+Inductive Eval : Graph * list Node * list Node -> Graph * list Node * list Node -> Prop :=
+| Eval_Step : forall s s', Step s s' -> Eval s s'
+| Eval_Term : forall s s', Term s -> Eval s s'.
+
+
+
+
+
+
 
 (* Size of the Graph *)
 Fixpoint get_size (g : Graph) : nat :=
