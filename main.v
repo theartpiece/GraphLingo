258365559565b00fa2333f6f0342d33277a6af10
Samridhi Roshan <samridhi.roshan2@gmail.com>
2024-04-23 16:16:16 -0500

    Combined 1 + 2

--- main.v
+++ main.v
@@ -3,9 +3,9 @@ Import ListNotations.
 (* Require Import Program.
 Require Import Wf. *)
 Require Import Program.Wf.
-(* Open Scope string_scope. *)
 Module Part1.
-(* ToDo: Should Node be [Definition Node] or [Notation Node]? Check HW1 to see what I am talking about *)
+
+(* Representation of Graph *)
 Notation Node := nat.
 Definition Edge := (prod Node Node).
 Definition EdgeList := (list Edge).
@@ -103,7 +103,7 @@ Fixpoint get_children (g: Graph) (u : Node): (list Node):=
                 if (Nat.eqb u v) 
                     then e 
                 else
-                    get_children g1 u
+                get_children g1 u
     end.
 
 
@@ -160,19 +160,140 @@ Proof.
     simpl. auto.
 Qed.
 
-(* Fixpoint is_reachable_in_k_steps (u v: Node) (k: nat): Prop :=
-    match k with
-    | 0 => if (Nat.eqb u v) then True else False
-    | S => 
-Fixpoint get_reachable_nodes_helper (g:Graph) (list_u: list Node) (list_seen : list Node): (list Node) :=
-    match list_u with 
-    | nil => nil
-    | v::l => if (existsb (Nat.eqb v) list_seen)
-                    then (get_reachable_nodes_helper g l list_seen)
-                else v::(get_reachable_nodes_helper g ((get_actual_neighbors g v) ++ l ) (v::list_seen))
-    end.
+Definition node_in (g: Graph) (n : Node) : Prop :=
+    exists edge_list, In (n, edge_list) g.
 
-Fixpoint is_cyclic_direc (g: Graph) : Prop := *)
+Example one_in_one_line:
+    node_in one_line 1.
+Proof.
+    simpl. unfold node_in. unfold one_line. simpl. eauto.
+Qed.
+
+Example three_not_in_one_line:
+    not (node_in one_line 3).
+Proof.
+    unfold not. intros. inversion H. inversion H0. easy. simpl in H1. inversion H1. easy. easy.
+Qed.
+
+(* RETURNS True if u->v in the graph g *)
+Definition edge_in (g: Graph) (u : Node) (v : Node) : Prop :=
+    forall ngbhrs_list, In (u, ngbhrs_list) g  ->  In v ngbhrs_list.
+
+(* g = [(1, [2,3,4]), (3, [4,5])] *)
+(* g = [(1, [2,3,4]), (3, [4,5]), (1, [5])]  -- this does not exist in our exmaplewas*) 
+(* edge_in g 1 2 = True *)
+(* edge_in g 2 1 = False *)
+
+    (* exists edge_list, (In (u, edge_list) g  /\  In v edge_list). *)
+
+Example one_two_in_one_line:
+    edge_in one_line 1 2.
+Proof.
+    (* simpl. unfold edge_in. unfold one_line. simpl. intros. split. destruct (1, edge_list). inversion H. easy. inversion H0. inversion H1. apply in_eq. easy.  *)
+    simpl. unfold edge_in. unfold one_line. simpl. intros. inversion H. easy. inversion H0. inversion H1. apply in_eq. easy. 
+Qed.
+
+Example two_one_not_in_one_line:
+    edge_in one_line 2 1 -> False.
+Proof.
+    intros .
+    unfold edge_in in H.
+    unfold one_line in H.
+    simpl in H.
+    specialize (H []).
+    firstorder.
+Qed.
+
+Example one_three_not_in_one_line:
+    edge_in one_line 1 3 -> False.
+Proof.
+    unfold not. simpl. unfold edge_in. unfold one_line. simpl. intros. eauto. specialize (H [2]). firstorder. easy.
+Qed.
+
+
+Inductive is_reachable_ind : Graph -> Node -> Node -> Prop :=
+| is_reachable_ind_base : forall v1 v2 g,edge_in g v1 v2 -> is_reachable_ind g v1 v2
+| is_reachable_ind_once : forall v1 v2 v3 g , edge_in g v1 v2 -> is_reachable_ind g v2 v3 -> is_reachable_ind g v1 v3.
+
+
+Example one_two_reachable_ind_in_one_line:
+    is_reachable_ind one_line 1 2.
+Proof.
+    eapply is_reachable_ind_base. eapply one_two_in_one_line. 
+Qed.
+
+Example one_one_not_reachable_ind_in_one_line:
+    is_reachable_ind one_line 1 1 -> False.
+Proof.
+    intros.
+Admitted.
+
+Example two_two_not_reachable_ind_in_one_line:
+    is_reachable_ind one_line 2 2 -> False.
+Proof.
+    intros.
+Admitted.
+
+
+Example one_cycle:=
+    construct_dir_graph 2 [(1,2);(2,1)].
+
+Example one_loop:=
+    construct_dir_graph 2 [(1,1)].
+
+Example one_one_reachable_in_one_cycle:
+    is_reachable_ind one_cycle 1 1.
+Proof.
+    eapply is_reachable_ind_once. unfold one_cycle. simpl. unfold edge_in. intros. inversion H. firstorder. easy. easy. firstorder.  easy. inversion H. firstorder.
+    eapply is_reachable_ind_base. unfold one_cycle. simpl. unfold edge_in. intros. inversion H. firstorder. inversion H. firstorder. easy. firstorder. easy.  easy.
+Qed.
+
+Example two_two_reachable_in_one_cycle:
+    is_reachable_ind one_cycle 2 2.
+Proof.
+    eapply is_reachable_ind_once. unfold one_cycle. simpl. unfold edge_in. intros. inversion H. firstorder. inversion H. firstorder. easy. firstorder. easy. firstorder.  easy.
+    eapply is_reachable_ind_base. unfold one_cycle. simpl. unfold edge_in. intros. inversion H. firstorder. inversion H. firstorder. easy. firstorder. easy. inversion H.  firstorder.
+Qed.
+
+
+
+
+Definition is_cyclic_ind (g: Graph) : Prop :=
+    exists v, node_in g v /\ is_reachable_ind g v v.
+    (* The following does not work because false is difficult to prove *)
+    (* exists v, node_in g v -> is_reachable_ind g v v. *)
+
+Example one_line_is_not_cyclic:
+    ~ (is_cyclic_ind one_line).
+Proof.
+    unfold is_cyclic_ind. unfold not. firstorder. inversion H; subst; clear H. apply two_two_not_reachable_ind_in_one_line in H0. firstorder. inversion H; subst; clear H. apply one_one_not_reachable_ind_in_one_line in H0. firstorder.
+Qed.
+
+
+Example one_cycle_is_cyclic:
+    is_cyclic_ind one_cycle.
+Proof.
+    exists 1. split. 
+    unfold node_in. unfold one_cycle. simpl. exists [2]. firstorder.
+    apply one_one_reachable_in_one_cycle.
+Qed.
+
+Definition is_dag (g:Graph) : Prop :=
+    is_cyclic_ind g -> False.
+
+
+
+Definition no_parent (g:Graph) (v:Node) : Prop :=
+    forall v1, node_in g v1 -> ~ edge_in g v1 v.
+
+Example one_has_noparent_in_one_line:
+    no_parent one_line 1.
+Proof.
+    unfold no_parent. firstorder. inversion H; subst; clear H. unfold not. intros. unfold one_line in H. unfold edge_in in H. simpl in H. specialize (H []). firstorder.
+    inversion H; subst; clear H.
+    unfold not. intros. unfold one_line in H. unfold edge_in in H. simpl in H. specialize (H [2]). firstorder. easy.
+Qed.
+
 
 (* ----------------------------------------------------------------------- *)
 
@@ -253,7 +374,9 @@ Lemma degree_add_edge : forall g v1 v2,
   (~ In v2 (get_children g v1)) -> degree (add_edge g v1 v2) v1 = S (degree g v1).
 Proof.
     intros.
-    destruct H.
+    destruct H. destruct get_children.
+    - simpl. 
+
 Admitted.
 
 (* Property of Graph: The sum of degrees in an undirected graph equals twice the number of edges *)
@@ -416,27 +539,42 @@ Proof.
     destruct H.
 Qed.
 
-Fixpoint Unique (l : list Node) : Prop :=
+(* see other way: bool*)
+(* Fixpoint Unique (l : list Node) : Prop :=
   match l with
   | [] => True
   | x :: xs => ~ In x xs /\ Unique xs
-  end.
+  end. *)
+(* Trying to implement as a bool so easier to proove in Coq*)
+Fixpoint Unique (l : list Node) : bool :=
+match l with
+  | [] => true
+  | x :: xs => if existsb (Nat.eqb x) xs then false else Unique xs
+end.
+
 
 Lemma bfs_unique_visited : forall (g : Graph) (source : Node),
-    Unique (bfs g source).
+    Unique (bfs g source) = true.
 Proof.
   intros g source.
   unfold Unique.
   induction (bfs g source).
   - auto.
-  - split.
+  - destruct (existsb (Nat.eqb a) l) eqn:Heq_existsb.
+    * apply existsb_exists in Heq_existsb.
+      destruct Heq_existsb as [x [Heq_x Hin_x]].
+      apply Nat.eqb_eq in Hin_x.
+      (* contradiction.
+      rewrite Heq_x in Hin_x.
+  fold Unique in *. eauto.  *)
+  (* split.
     * induction l.
       ** simpl. auto.
       ** simpl. intros [Heq | Hin].
         *** rewrite Heq in IHl. unfold Unique in IHl. eauto.  
         admit.
-        *** unfold Unique in IHl0. admit.
-    * apply IHl.
+        *** fold Unique in IHl0. fold Unique in IHl.  admit.
+    * apply IHl. *)
 Admitted.
 
 Lemma bfs_terminates : forall (g : Graph) (source : Node),
@@ -530,3 +668,126 @@ Definition example_diameter_graph : Graph := construct_dir_graph 7 [(1, 2); (2, 
 Example diameter_longest_path : 
  diameter example_diameter_graph = 3. 
 Proof. reflexivity. Qed.
+
+
+(* Additional Properties and some extensions for Graph *)
+
+(* Walk in a graph *)
+Inductive is_walk: Graph -> list Node -> Prop :=
+| walkOne : forall  g v, node_in g v -> is_walk g [v]
+| walkOneStep : forall g u v l, edge_in g u v -> is_walk g (v::l) -> is_walk g (u :: v :: l).
+
+Inductive has_no_duplicates: list Node -> Prop :=
+| emptyNoDuplicates : has_no_duplicates []
+| nonEmptyNoDuplicates: forall v l, not (In v l) -> has_no_duplicates (v::l).
+
+Fixpoint tail_elem (l : list Node) :=
+  match l with
+  | [] => error
+  | [x] => Some x
+  | _ :: xs => tail_elem xs
+  end.
+
+Goal tail_elem [2;3;4] = Some 4.
+Proof.
+firstorder.
+Qed.
+
+Goal head [2;3;4] = Some 2.
+Proof.
+firstorder.
+Qed.
+
+(* Inductive head_tail_same : list Node -> Prop :=
+| emptyHeadTailSame : head_tail_same []
+| nonEmptyHeadTailSame: forall a l, head_tail_same (a::(l++[a])).
+
+Compute (1::([2;3;4]++[5])).
+
+Goal head_tail_same [1;2;1].
+(* Goal head_tail_same (1::([2]++[1])). *)
+Proof.
+eapply nonEmptyHeadTailSame. *)
+
+
+Definition head_tail_same (l : list Node) :=
+    length l >= 2 /\ ((head l) = (tail_elem l)).
+
+Goal head_tail_same [2;3;2].
+Proof.
+unfold head_tail_same. split. unfold length. lia. firstorder.
+Qed.
+
+    
+Definition is_path (g:Graph) (l: list Node) : Prop :=
+    is_walk g l /\ has_no_duplicates l.
+
+
+Definition is_cycle (g:Graph) (l: list Node) : Prop :=
+        is_walk g l /\ head_tail_same l.
+
+Lemma head_tail_same_implies_duplicates:
+    forall l, head_tail_same l -> not ( has_no_duplicates l).
+Proof.
+induction l.
+- unfold head_tail_same. intros. destruct H. simpl in H. firstorder. easy.
+-intros. destruct H. 
+Admitted.
+
+Lemma path_is_not_cycle: 
+    forall g l, is_path g l -> not (is_cycle g l).
+Proof.
+    (* induction l.
+    - unfold not. intros. inversion H; subst; clear H. inversion H0; subst; clear H0. unfold head_tail_same in H3. inversion H3. simpl in H0. lia.
+    - intros. unfold not.  intros. inversion H . eapply walkOneStep in H1. inversion H2. simpl in H. *)
+    intros. unfold not, is_path, is_cycle. destruct H. intros. destruct H1. eapply head_tail_same_implies_duplicates in H0. easy. easy.
+Qed.
+
+Lemma maximal_path_exists_in_dag:
+    forall g l , is_dag g -> (exists max_path, (is_path g max_path) /\ ((is_path g l) -> (length l <= length max_path ))).
+Proof.
+Admitted.
+
+
+    
+        
+(* The following theorem requires knowledge that a path of maximum length occurs in a dag *)
+Theorem dag_has_atleast_one_root: 
+    forall g, is_dag g -> (exists v, node_in g v /\ no_parent g v).
+Proof.
+    intros. firstorder. 
+Admitted.
+
+(* Forest *)
+
+Definition is_forest (g:Graph) : Prop :=
+    is_dag g /\ (forall p1 p2 v, node_in g v -> (edge_in g p1 v -> edge_in g p2 v  -> p1 = p2)).
+    
+Example is_line_is_forest:
+    is_forest one_line.
+Proof.
+    split.
+    unfold is_dag. apply one_line_is_not_cyclic.
+    unfold one_line. simpl. unfold edge_in. intros. firstorder. inversion H; subst; clear H. unfold edge_in in H1. destruct p1.
+Admitted.
+
+Definition is_connected_ind (g: Graph) : Prop :=
+ forall u v, (not (u = v) /\ node_in g u /\ node_in g v) -> (is_reachable_ind g u v  \/ is_reachable_ind g v u).
+
+
+(* Is Graph a Tree *)
+
+Definition is_tree (g: Graph):
+    is_forest g /\ is_connected_ind g.
+
+
+
+Definition is_leaf (g:Graph) (v: Node) :Prop :=
+    (node_in g v) /\ (In (v, []) g).
+
+Proof.
+Admitted.
+
+
+Theorem tree_has_1_less_internal_nodes_than_leaves:
+    True.
